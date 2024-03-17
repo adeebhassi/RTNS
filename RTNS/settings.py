2133eaf63c94959293ba8a7a8fc657a9b737f397
unknown <iamslayer786@gmail.com>
2024-03-17 22:56:54 +0500

    comtypes changes

--- RTNS/settings.py
+++ RTNS/settings.py
@@ -168,6 +168,6 @@ ALLOWED_HOSTS = ['web-production-eba5.up.railway.app']
 LOGIN_URL = 'user_auth:user_signin'
 
 GOOGLE_DRIVE_CREDENTIALS = os.path.join(BASE_DIR, 'rtns-413207-168420d2b7cd.json')
-
+CSRF_TRUSTED_ORIGINS=['https://web-production-eba5.up.railway.app']
 
 
