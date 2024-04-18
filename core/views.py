ced70c829b5a26777be00c673ae9b9383abd98ff
unknown <iamslayer786@gmail.com>
2024-04-18 10:17:24 +0500

    final commit

--- core/views.py
+++ core/views.py
@@ -6,38 +6,50 @@ from .models import *
 # Create your views here.
 from datetime import datetime, timedelta
 
+from datetime import date, datetime
+from django.core.exceptions import ObjectDoesNotExist
+from .models import SliderImage, AboutUs, Patron, Gallery
+from events.models import Event, Speech
+
+
+# Create your views here.
+
 def index(request):
     today = date.today()
     try:
         slider_images = SliderImage.objects.all()
         about_us = AboutUs.objects.first()
         patron = Patron.objects.all()
-        gallery=Gallery.objects.all()
+        gallery = Gallery.objects.all()
+
         upcoming_event = Event.objects.filter(date__gte=today).order_by('date').first()
+        print("event",upcoming_event)
         if upcoming_event:
-            earliest_speech = Speech.objects.earliest('start_time')
-            event_date = upcoming_event.date
             first_speech = Speech.objects.filter(event=upcoming_event).order_by('start_time').first()
             if first_speech:
                 event_date_formatted = upcoming_event.date.strftime("%Y-%m-%d")
                 event_time = first_speech.start_time.strftime("%H:%M:%S")
-                event_datetime = datetime.combine(event_date, first_speech.start_time)
+                event_datetime = datetime.combine(upcoming_event.date, first_speech.start_time)
                 current_datetime = datetime.now()
                 remaining_days = (event_datetime - current_datetime).days
-                contaxt = {
-                    'event_date': event_date,
-                    'event_time': event_time,
+                context = {
                     'event_date_formatted': event_date_formatted,
+                    'event_time': event_time,
                     'remaining_days': remaining_days,
-                    'gallery':gallery
                 }
-                return render(request, 'core/index.html', contaxt)
-    except ObjectDoesNotExist:
-        pass
+                print("contetn",context)
+            else:
+                context = {}
+        else:
+            context = {}
 
-    home_content = {
-        'slider_images': slider_images,
-        'about_us': about_us,
-        'patron': patron,
-    }
-    return render(request, 'core/index.html', home_content)
+        home_content = {
+            'slider_images': slider_images,
+            'about_us': about_us,
+            'patron': patron,
+            'gallery': gallery,
+        }
+        print("conte2",context)
+        return render(request, 'core/index.html', {**home_content, **context})
+    except ObjectDoesNotExist:
+        return render(request, 'core/index.html', {})
